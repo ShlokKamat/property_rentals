f2bf5e7a05799e3088ff3174de18fdf28f3a0403
I524910 <shlok.sanjay.kamat@sap.com>
2024-05-29 15:59:59 +0530

    Utils Update

--- app/src/main/java/com/example/rentalapp/Utils.java
+++ app/src/main/java/com/example/rentalapp/Utils.java
@@ -1,6 +1,7 @@
 package com.example.rentalapp;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,8 +9,20 @@ import java.util.Objects;
 
 public class Utils {
 
+    static final String PROPERTY_PARCEL_KEY = "property_parcel_key";
     static float[] means = new float[]{970.90830822f, 2.19974171f, 4.11493758f, 1.7572105f};
     static float[] stds = new float[]{689.26193329f, 3.68153938f, 4.34937374f, 0.89890801f};
+    static final int MAX_IMAGE_SIZE = 500 * 1024; //500 KB
+    static String chooseEmailTitle = "Select app to send e-mail";
+    static String emailSubject = "BroKar Rent: Property Enquiry";
+
+    public static String getEmailBody(String propertyName,String propertyBhk, String propertyLocality, String myName) {
+        if(myName.equals("")){
+            return "Hey,\nI am interested in your "+propertyBhk+" property "+propertyName+" in "+propertyLocality+".\nRegards,\n<Enter Your Name>";
+        } else {
+            return "Hey,\nI am interested in your "+propertyBhk+" property "+propertyName+" in "+propertyLocality+".\nRegards,\n"+myName;
+        }
+    }
 
     public static float[] scaleInputForModel(float[] inputArray) {
         float[] scaledInput = new float[inputArray.length];
@@ -35,45 +48,57 @@ public class Utils {
 
     public static String getUserName() {
         FirebaseAuth auth = FirebaseAuth.getInstance();
-        return Objects.requireNonNull(auth.getCurrentUser()).getDisplayName();
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+        if (currentUser != null) {
+            return currentUser.getDisplayName();
+        } else {
+            return "";
+        }
     }
 
     public static String getUserEmail() {
         FirebaseAuth auth = FirebaseAuth.getInstance();
-        return Objects.requireNonNull(auth.getCurrentUser()).getEmail();
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+        if (currentUser != null) {
+            return currentUser.getEmail();
+        } else {
+            return "";
+        }
     }
 
 
     public static float[] setInputArrayFromProperty(PropertyDataClass propertyData) {
 
         Map<String, Integer> bhk_type_map = new HashMap<>();
-        bhk_type_map.put("1 RK",0);
-        bhk_type_map.put("1 BHK",1);
-        bhk_type_map.put("2 BHK",2);
-        bhk_type_map.put("3 BHK",3);
-        bhk_type_map.put("4 BHK",4);
+        bhk_type_map.put("1 RK", 0);
+        bhk_type_map.put("1 BHK", 1);
+        bhk_type_map.put("2 BHK", 2);
+        bhk_type_map.put("3 BHK", 3);
+        bhk_type_map.put("4 BHK", 4);
 
         Map<String, Integer> property_age_map = new HashMap<>();
-        property_age_map.put("0 – 1 years",0);
-        property_age_map.put("1 – 3 years",1);
-        property_age_map.put("3 – 5 years",2);
-        property_age_map.put("5 – 10 years",3);
-        property_age_map.put("> 10 years",4);
+        property_age_map.put("0 – 1 years", 0);
+        property_age_map.put("1 – 3 years", 1);
+        property_age_map.put("3 – 5 years", 2);
+        property_age_map.put("5 – 10 years", 3);
+        property_age_map.put("> 10 years", 4);
 
         Map<String, Integer> furnishing_map = new HashMap<>();
-        furnishing_map.put("Unfurnished",0);
-        furnishing_map.put("Semi Furnished",1);
-        furnishing_map.put("Fully Furnished",2);
+        furnishing_map.put("Unfurnished", 0);
+        furnishing_map.put("Semi Furnished", 1);
+        furnishing_map.put("Fully Furnished", 2);
 
         Map<String, Integer> parking_map = new HashMap<>();
-        parking_map.put("No",0);
-        parking_map.put("Bike",1);
-        parking_map.put("Car",2);
-        parking_map.put("Bike & Car",3);
+        parking_map.put("No", 0);
+        parking_map.put("Bike", 1);
+        parking_map.put("Car", 2);
+        parking_map.put("Bike & Car", 3);
 
         Map<String, Integer> security_map = new HashMap<>();
-        security_map.put("No",0);
-        security_map.put("Yes",1);
+        security_map.put("No", 0);
+        security_map.put("Yes", 1);
 
         float[] inputArray = new float[]{
                 (float) propertyData.getPropertySize(), //Size
